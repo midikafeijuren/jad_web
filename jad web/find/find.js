efce5b26bed5728115dc43cc74db3096aeac2722
梁津硕 <yonghumeng@163.com>
2024-07-28 14:33:31 +0800

    Add files via upload

---
+++ jad web/find/find.js
@@ -0,0 +1,23 @@
+// app.js
+const data = {
+    "pink cup(粉红杯)": "project/pinkcup/project_pinkcup.html",
+    "hkdog": "project/hkdog/project_hkdog.html"
+    
+};
+
+document.getElementById('searchInput').addEventListener('input', function(e) {
+    const searchQuery = e.target.value.toLowerCase();
+    const resultsContainer = document.getElementById('searchResults');
+    resultsContainer.innerHTML = ''; // Clear previous results
+
+    Object.keys(data).forEach(function(key) {
+        if (key.toLowerCase().includes(searchQuery)) {
+            const listItem = document.createElement('li');
+            listItem.textContent = key;
+            listItem.addEventListener('click', function() {
+                window.location.href = data[key];
+            });
+            resultsContainer.appendChild(listItem);
+        }
+    });
+});
